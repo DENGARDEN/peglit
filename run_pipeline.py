20381db1d67934df86b038f500599fc238de9dcc
DENGARDEN <poowooho3@g.skku.edu>
2023-09-14 10:57:35 +0900

    2023.09.14

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,12 +1,19 @@
-DATA_DIR = "2528(2).csv"
+DATA_DIR = "./4k_small_lib_epeg-2023-09-13_22-44-10.csv"
+SPCAS9_SCAFFOLD = (
+    "GTTTTAGAGCTAGAAATAGCAAGTTAAAATAAGGCTAGTCCGTTATCAACTTGAAAAAGTGGCACCGAGTCGGTGC"
+)
+
+OPTIMIZED_SGRNA_SCAFFOLD = "GTTTcAGAGCTAtgctgGAAAcagcaTAGCAAGTTgAAATAAGGCTAGTCCGTTATCAACTTGAAAAAGTGGCACCGAGTCGGTGC"  # doi://10.1186/s13059-015-0846-3 Fig. 2b T2C-4
+tevopreQ1_DP = "CGCGGTTCTATCTAGTTACGCGTTAAACCAACTAGAA"
+LINKER_PATTERN = "NNNNNNNN"
 
 
 def callback(data: dict):
     # tentative attribute design
     linker = peglit.pegLIT(
-        seq_spacer=data["spacer"],
-        seq_scaffold=data["scaffold"],
-        seq_template=data["template"],
+        seq_spacer=data["Guide"],
+        seq_scaffold=data["Scaffold"],
+        seq_template=data["RTT"],
         seq_pbs=data["PBS"],
         linker_pattern=data["linker_pattern"],
         seq_motif=data["motif"],
@@ -23,10 +30,19 @@ if __name__ == "__main__":
     import pandas as pd
     from tqdm import tqdm
 
+    tqdm.pandas()
     batch_size = mp.cpu_count()
-    # batch_size =1 
+    # batch_size =1
 
     # Read CSV and chunk it
+    # Some modification for pe6 case
+    df = pd.read_csv(DATA_DIR)
+    # df["linker_pattern"] = str(LINKER_PATTERN)
+    # df["motif"] = tevopreQ1_DP
+    df["Scaffold"] = OPTIMIZED_SGRNA_SCAFFOLD
+    df.to_csv(DATA_DIR)
+    del df
+
     df_gen = pd.read_csv(DATA_DIR, chunksize=batch_size)
     # pooling peglit processes
     results = []
@@ -34,12 +50,11 @@ if __name__ == "__main__":
         data = list(df_subset.to_dict("index").values())
         with ProcessPoolExecutor(max_workers=batch_size) as executor:
             rvals = list(executor.map(callback, data))
-            results.extend(
-                rvals)
+            results.extend(rvals)
 
         # print("Done with a batch")
 
-    pd.DataFrame.from_dict(results).rename(columns={"linker_pattern":"linker"}).to_csv(
+    pd.DataFrame.from_dict(results).rename(columns={"linker_pattern": "linker"}).to_csv(
         f"{DATA_DIR}_pegLIT_results.csv",
         index=False,
     )
